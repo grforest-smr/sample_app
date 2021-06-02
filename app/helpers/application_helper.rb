818111038dfa395cfe99c671f299bf99158b115f
grforest <grforest@yandex.ru>
2021-06-02 15:33:18 +0400

    edit footer, title and insert info on static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Возвращает полный заголовок на основе заголовка страницы.
   def full_title(page_title = '')
-    base_title = "GRF"
+    base_title = "Anti-realtor"
     if page_title.empty?
       base_title
     else
